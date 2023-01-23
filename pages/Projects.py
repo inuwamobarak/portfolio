140977a6d14c9d7d63ab66008f7b05e7a1939089
Inuwa Mobarak Abraham <inuwamobarak@gmail.com>
2023-01-23 22:50:52 +0100

    Update Projects.py

--- pages/Projects.py
+++ pages/Projects.py
@@ -1,6 +1,13 @@
 import streamlit as st
 st.title('Inuwa Mobarak Abraham') #setting title
 
+html_text2 = '<hr>'
+    st.markdown(html_text2, unsafe_allow_html=True)
+    
+    st.write('This is a prototype for predicting employee attrition using machine learning and also using cloud infrastructure')
+    st.subheader('(Please fill in the Employee details accordingly)')
+
+
 col1, col2, col3 = st.columns(3)
 
 with col1:
