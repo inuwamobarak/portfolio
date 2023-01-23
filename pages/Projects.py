d9380d73c2bca1e245f1f2d317961c33fd1cf8bc
Inuwa Mobarak Abraham <inuwamobarak@gmail.com>
2023-01-24 00:09:15 +0100

    Update Projects.py

--- pages/Projects.py
+++ pages/Projects.py
@@ -16,7 +16,7 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-add_bg_from_local('images/pexels-steve-johnson-1843717.jpg')
+add_bg_from_local('images/pexels-andreea-ch-1166643.jpg')
 
 #setting title
 st.title("Inuwa Mobarak Abraham")
