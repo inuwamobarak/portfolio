d659fd5d8de24b3181b598dfe7694d0fd4e307f5
Inuwa Mobarak Abraham <inuwamobarak@gmail.com>
2023-01-23 22:37:36 +0100

    Update Projects.py

--- pages/Projects.py
+++ pages/Projects.py
@@ -3,13 +3,30 @@ import streamlit as st
 col1, col2, col3 = st.columns(3)
 
 with col1:
-   st.header("A cat")
+   st.header("Asphalt Pavement Degradation")
    st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col2:
-   st.header("A dog")
+   st.header("Colombian Fake Banknotes Detection")
    st.image("https://static.streamlit.io/examples/dog.jpg")
 
 with col3:
-   st.header("An owl")
+   st.header("Employee Attrition Prediction")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
+
+
+#next coloumns below
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Asphalt Pavement Degradation")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("Colombian Fake Banknotes Detection")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("Employee Attrition Prediction")
    st.image("https://static.streamlit.io/examples/owl.jpg")
