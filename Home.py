3422bfe0d5716eda0d07c5052340be74b17a2ca7
Inuwa Mobarak Abraham <inuwamobarak@gmail.com>
2023-01-23 22:44:01 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,23 @@
 import streamlit as st
+#setting background
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:images/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('images/bg2.jpg')
 
+
+#setting columns
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 with tab1:
